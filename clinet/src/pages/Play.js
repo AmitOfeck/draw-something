83eb1eeafbad0c515c11415a17ed43efe9e96fd7
Amit Ofeck <Amitofeck1@gmail.com>
2022-01-11 16:46:58 +0200

    router

---
+++ clinet/src/pages/Play.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Play(props) {
+    return (
+        <div>
+            <h2>play</h2>
+        </div>
+    );
+}
+
+export default Play;
---
+++ clinet/src/pages/Routes/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import Play from '../Play';
+
+function Router() {
+    return (
+        <div>
+            <Routes>
+            <Route path="/" exact element={<Play/>}/>
+            <Route path="/:UserName/:UserId/:GameId" exact element={<Play/>}/>
+            </Routes>
+        </div>
+    );
+}
+
+export default Router;
