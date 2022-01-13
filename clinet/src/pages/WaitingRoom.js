7729c6574fab88336c05e1a748da80cefa614d05
Amit Ofeck <Amitofeck1@gmail.com>
2022-01-13 14:02:25 +0200

    css

--- clinet/src/pages/WaitingRoom.js
+++ clinet/src/pages/WaitingRoom.js
@@ -7,7 +7,7 @@ function WaitingRoom(props) {
             <h1>Waiting Room...</h1>
             <br/>
             <div className="spinner-border text-primary" role="status">
-              <span class="visually-hidden">Loading...</span>
+            <span className="visually-hidden">Loading...</span>
             </div>
         </div>
     );
