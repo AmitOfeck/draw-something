0538d7776c5e46d8cca2fcd0b2192d8c5ed86a38
Amit Ofeck <Amitofeck1@gmail.com>
2022-01-15 18:41:21 +0200

    add mongo explicit

--- server/configs/dataBase.js
+++ server/configs/dataBase.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/gamesDB',{
+require('dotenv').config()
+
+const mongoReomoteUrl = "mongodb+srv://amitofeck:080300@cluster0.ornxe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const localurl = 'mongodb://localhost:27017/gamesDB'
+
+mongoose.connect(mongoReomoteUrl,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
